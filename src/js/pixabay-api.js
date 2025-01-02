352d82f2f9ca821f95c6bc650fb57e52ddfa2e67
Rodion <89980@ukr.net>
2025-01-03 01:34:13 +0200

    hwjs11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,11 +1,18 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const apiUrl = 'https://pixabay.com/api/';
+
 const apiKey = '47996916-063d7568314d3761f2b5e2fb9';
 const url = 'https://pixabay.com/api/';
 const pictureName = document.querySelector('#picture-picker');
 
 const searchButton = document.querySelector('#Search-btn');
+const resultContent = document.querySelector('#results');
 
 searchButton.addEventListener('click', () => {
   const searchedPicture = pictureName.value.trim();
@@ -15,6 +22,7 @@ searchButton.addEventListener('click', () => {
       title: 'Error',
       message: `Please enter picture description`,
     });
+    return;
   }
 
   const params = new URLSearchParams({
@@ -25,7 +33,9 @@ searchButton.addEventListener('click', () => {
     safesearch: 'true',
   });
 
-  fetch(`${url}?${params.toString()}`, {
+  resultContent.innerHTML = '';
+
+  fetch(`${proxyUrl}${apiUrl}?${params.toString()}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -38,7 +48,57 @@ searchButton.addEventListener('click', () => {
       return response.json();
     })
     .then(data => {
-      console.log(data);
+      if (data.hits.length === 0) {
+        iziToast.info({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+        return;
+      }
+
+      data.hits.forEach(hit => {
+        const card = createImageCard(hit);
+        resultContent.appendChild(card);
+      });
+
+      const lightbox = new SimpleLightbox('.lightbox-item');
+      lightbox.refresh();
     })
     .catch(error => console.log(error));
+
+  pictureName.value = '';
 });
+
+function createImageCard(hit) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const link = document.createElement('a');
+  link.href = hit.largeImageURL;
+  link.classList.add('lightbox-item');
+
+  const image = document.createElement('img');
+  image.src = hit.webformatURL;
+  image.alt = hit.tags;
+
+  link.setAttribute('data-title', hit.tags);
+
+  link.appendChild(image);
+
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+
+  const stats = document.createElement('p');
+  stats.innerHTML = `
+    <span>Likes: ${hit.likes}</span> | 
+    <span>Views: ${hit.views}</span> | 
+    <span>Comments: ${hit.comments}</span> | 
+    <span>Downloads: ${hit.downloads}</span>
+  `;
+
+  cardInfo.appendChild(stats);
+  card.appendChild(link);
+  card.appendChild(cardInfo);
+
+  return card;
+}
